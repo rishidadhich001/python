ec0b41c1111a7171ff172bc807730c217fe1d6fe
Rishi Dadhich <156800173+rishidadhich001@users.noreply.github.com>
2024-05-08 00:32:49 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,9 @@
+a=int(input("enter first value:"))
+b=int(input("enter second  value:"))
+c=int(input("enter third value:"))
+if(a>=b and a>=c):
+    print("a is greater")
+elif(b>c):
+    print("b is greater")
+else:
+    print("c is greater")  
---
+++ 5.py
@@ -0,0 +1,6 @@
+x=int(input("enter number:"))
+
+if(x%5==0):
+    print("it divisible by 5")
+else:
+    print("it not divisible by 5")
